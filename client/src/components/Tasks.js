05f44cb8622158728cd644377d93b60c99273c36
Apollo <rapollo@protonmail.com>
2021-12-21 02:56:16 +0100

    adding toggle btn

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -5,21 +5,40 @@ import "../styles/tasks.css"
 
 const Tasks = () => {
     const [fetchedTasks, updateFetchedTasks] = useState([])
+    const [addActivated, setAddActivated] = useState(false)
 
     const fetchTasks = async () => {
         const res = await Axios.get("http://localhost:5000/todo")
         updateFetchedTasks(res.data)
     }
 
-    useEffect(() => {
-    }, [fetchedTasks])
+    const toggleAddButton = () => {
+        if (addActivated) setAddActivated(false)
+        else setAddActivated(true)
+    }
 
-    return(
-        <div className="tasklist">
-            {fetchedTasks.map(task => <Task task={task} />)}
-            <button className="add btn" onClick={() => fetchTasks()}>Test</button>
-        </div>
-    )
+    //first render
+    useEffect(() => {fetchTasks()}, [])
+
+    //on adding new Data
+    useEffect(() => {
+    }, [fetchedTasks, addActivated])
+
+    if (!addActivated) {
+        return(
+            <div className="tasklist">
+                {fetchedTasks.map(task => <Task task={task} />)}
+                <button className="add btn" onClick={() => toggleAddButton()}>Test</button>
+            </div>
+        )
+    } else {
+        return(
+            <div className="tasklist">
+                {fetchedTasks.map(task => <Task task={task} />)}
+                <button className="add btn" onClick={() => toggleAddButton()}>Test2</button>
+            </div>
+        )
+    }
 }
 
 export default Tasks
